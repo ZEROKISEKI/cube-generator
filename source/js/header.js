3530f3b990c27a151ecdc39ba6f5b139a81fa62f
aonosora <20142100095@m.scnu.edu.cn>
2017-08-18 16:36:26 +0800

    remove background repeat loading

--- source/js/header.js
+++ source/js/header.js
@@ -49,46 +49,35 @@ import $ from 'jquery'
     async function loadImage() {
         return new Promise((resolve, reject) => {
 
+            const width = headerModule.image.width
+            const height = headerModule.image.height
+
             const background = $('.cube-header-background')
+            const body = $('body')
 
             if (clientWidth >= 768 && background.length > 0) {
                 const content = $('div.cube-content')
                 const menu = $('nav.cube-menu')
                 const typed = $('header.cube-background div.cube-type')
 
-                const backgroundImage = background.css('background-image')
                 let position = background.css('background-position').split(' ')[1]
                 position = /px/.test(position) ? +position.split('px')[0] : position
 
-                const clientWidth = $('body').width()
+                const indeedHeight = Math.round(clientWidth * height / width)
+                const indeedPosition = Math.round(position * indeedHeight / height)
+                background.css('height', `${indeedHeight + indeedPosition}px`)
+                background.css('background-position', `center ${indeedPosition}px`)
+                background.trigger('resize')
+                $('div.load-header-background').css('display', 'none')
+                background.css('visibility', 'visible')
+                typed.css('top', `${Math.round((indeedHeight + indeedPosition + 44) / 2) - 28}px`)
+                menu.append(`<style>nav#cube-top-memu:before { background-position: center ${indeedPosition + 44}px;visibility: visible; }</style>`)
+                content.css('visibility', 'visible')
+                body.css('overflow', 'auto')
+                resolve()
 
-                // fix browser different url str
-                const url = /"/.test(backgroundImage) ?
-                    backgroundImage.substr(5, backgroundImage.length - 7) :
-                    backgroundImage.substr(4, backgroundImage.length - 5)
-
-                let backgroundImg = new Image()
-
-                backgroundImg.src = url
-
-                backgroundImg.onload = function (event) {
-                    const width = event.target.width
-                    const height = event.target.height
-                    const indeedHeight = Math.round(clientWidth * height / width)
-                    const indeedPosition = Math.round(position * indeedHeight / height)
-                    background.css('height', `${indeedHeight + indeedPosition}px`)
-                    background.css('background-position', `center ${indeedPosition}px`)
-                    background.trigger('resize')
-                    $('div.load-header-background').css('display', 'none')
-                    background.css('visibility', 'visible')
-                    typed.css('top', `${Math.round((indeedHeight + indeedPosition + 44) / 2) - 28}px`)
-                    menu.append(`<style>nav#cube-top-memu:before { background-position: center ${indeedPosition + 44}px;visibility: visible; }</style>`)
-                    content.css('visibility', 'visible')
-                    $('body').css('overflow', 'auto')
-                    resolve()
-                }
             } else {
-                $('body').css('overflow', 'auto')
+                body.css('overflow', 'auto')
                 resolve()
             }
         })
